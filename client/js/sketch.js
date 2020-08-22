38ba997b4d979ed2c6a715f85db2e6f4822e1de5
CristobalFalso <none>
2020-08-22 11:55:59 -0400

    Objeto Player con atributos incluido, iniciando parametros en base al objeto

--- client/js/sketch.js
+++ client/js/sketch.js
@@ -1,9 +1,6 @@
 var socket;
-
-var x = 0;
-var y = 50;
-var speedX = 0;
-var speedY = 0;
+var playerNum1;
+var playerNum2;
 
 function preload()
 {
@@ -19,48 +16,48 @@ function setup()
         console.log(data);
     });
 
+    playerNum1 = new Player('FakeCris',0,50);
+
+
+    playerNum2 = new Player('FakeCris2',40,110);
+
+    createCanvas(windowWidth, windowHeight);
+    
     
 }
 
 function draw()
 {
-    createCanvas(windowWidth, windowHeight);
+    
     background(51,51,255);
 
-    translate(width/2 - x, height/2 - y);
+    translate(width/2 - playerNum1.x, height/2 - playerNum1.y);
 
     fill(51);
     rect(0,0,600,600);
+    
+    playerNum1.draw();
+    playerNum2.draw();
 
-    fill(255,50,50);
-    beginShape();
-    vertex(x + 0,y + 0);
-    vertex(x + (-30),y + 90);
-    vertex(x + 0, y + 75);
-    vertex(x + 30, y + 90);
-    endShape(CLOSE);
-
-    x = x + speedX;
-    y = y + speedY;
 }
 
 function keyPressed() 
 {
   if(key == "w")
   {
-    speedY = -1;
+    playerNum1.speedY = -5;
   }
   if(key == "s")
   {
-    speedY = 1; 
+    playerNum1.speedY = 5; 
   }
   if(key == "a")
   {
-    speedX = -1; 
+    playerNum1.speedX = -5; 
   }
   if(key == "d")
   {
-    speedX = 1;  
+    playerNum1.speedX = 5;  
   }
 }
 
@@ -68,18 +65,45 @@ function keyReleased()
 {
   if(key == "w")
   {
-    speedY = 0;
+    playerNum1.speedY = 0;
   }
   if(key == "s")
   {
-    speedY = 0;
+    playerNum1.speedY = 0;
   }
   if(key == "d")
   {
-    speedX = 0;
+    playerNum1.speedX = 0;
   }
   if(key == "a")
   {
-    speedX = 0;
+    playerNum1.speedX = 0;
   }
+}
+
+var Player = function(name, x, y)
+{
+  this.name = name;
+  this.x = x;
+  this.y = y;
+  this.speedX = 0;
+  this.speedY = 0;
+
+  this.draw = function()
+  {
+    
+
+    fill(255,50,50);
+    beginShape();
+    vertex(this.x + 0,this.y + 0);
+    vertex(this.x + (-30),this.y + 90);
+    vertex(this.x + 0, this.y + 75);
+    vertex(this.x + 30, this.y + 90);
+    endShape(CLOSE);
+
+    this.x = this.x + this.speedX;
+    this.y = this.y + this.speedY; 
+  }
+
+  return this;
 }
